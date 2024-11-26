c6d80a60fdd8f62f14dd4c31199b3ce9eefb6900
lesley <lesleybanadzem2017@gmail.com>
2024-11-25 23:40:34 -0600

    Added route to update movie

--- routes/movieRouter.go
+++ routes/movieRouter.go
@@ -14,4 +14,6 @@ func MovieRoutes(router *gin.Engine) {
 	router.POST("/movies/create-movie", controllers.CreateMovie())
 	router.GET("/movies/:movie_id", controllers.GetMovie())
 	router.GET("/movies", controllers.GetAllMovies())
+
+	router.PUT("/movies/:movie_id", controllers.UpdateMovie())
 }
